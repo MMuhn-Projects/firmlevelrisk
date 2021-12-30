273666083e573feceb02b19d7c448ee8d4fd18a1
tbbarr <42769253+tbbarr@users.noreply.github.com>
2021-12-30 16:21:06 -0600

    Modify code for our data/setting (#1)
    Applied Black formatting
    
    Simple cleanup
    
    Cleanup _helpers
    - Removed header
    
    Added helper function for parsing case file names
    - Function returns the case id and opinion_num.
    
    Separated text preprocessing function
    
    Updated gitignore to ignore input and outputs
    
    Updated main code to work with cases texts
    
    Finalised main code to run

--- code/_helpers.py
+++ code/_helpers.py
@@ -1,152 +1,165 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Aug 23 21:18:19 2020
-
-@author: markusschwedeler
-"""
-
 from bs4 import BeautifulSoup, FeatureNotFound
 import os
 import re
 import pandas as pd
+import re
 
 # Note: need to install html5lib parser
 
 
-
 def import_sentimentwords(file):
-    df = pd.read_csv(file, sep=',')
-    tokeep = ['Word','Positive']
-    positive = set([x['Word'].lower() for idx, x in df[tokeep].iterrows()
-                    if x['Positive'] > 0])
-    tokeep = ['Word','Negative']
-    negative = set([x['Word'].lower() for idx, x in df[tokeep].iterrows()
-                    if x['Negative'] > 0])
-    return {'positive':positive, 'negative':negative}
-
+    df = pd.read_csv(file, sep=",")
+    tokeep = ["Word", "Positive"]
+    positive = set(
+        [x["Word"].lower() for idx, x in df[tokeep].iterrows() if x["Positive"] > 0]
+    )
+    tokeep = ["Word", "Negative"]
+    negative = set(
+        [x["Word"].lower() for idx, x in df[tokeep].iterrows() if x["Negative"] > 0]
+    )
+    return {"positive": positive, "negative": negative}
 
 
 def import_riskwords(file):
     synonyms = set()
-    with open(file, 'r') as inp:
+    with open(file, "r") as inp:
         for line in inp:
-            split = line.split(' ')
+            split = line.split(" ")
             for syn in split:
-                synonyms.add(re.sub('\n', '', syn))
+                synonyms.add(re.sub("\n", "", syn))
     return synonyms
 
 
-
 def import_politicalbigrams(file):
-    df = pd.read_csv(file, sep=',', encoding='utf-8')
-    df = df.assign(bigram=df['bigram'].str.replace('_', ' '))
-    df.rename(columns={'politicaltbb':'tfidf'}, inplace=True)
-    df.set_index('bigram', inplace=True)
-    return df.to_dict(orient='index')
-
+    df = pd.read_csv(file, sep=",", encoding="utf-8")
+    df = df.assign(bigram=df["bigram"].str.replace("_", " "))
+    df.rename(columns={"politicaltbb": "tfidf"}, inplace=True)
+    df.set_index("bigram", inplace=True)
+    return df.to_dict(orient="index")
 
 
 def load_transcripts(folder):
-    
+
     # ATTENTION: Assumes html file was downloaded from The Motley Fool
-    
+
     # Collect files in list
-    files = [x for x in os.listdir(folder) if '.html' in x]
-    
+    files = [x for x in os.listdir(folder) if ".html" in x]
+
     # Collect results here
     results = {}
-    
+
     # Loop through all files
     for file in files:
-        
+
         # Load HTML
-        with open(folder + file, 'r', encoding='utf-8') as infile:
+        with open(folder + file, "r", encoding="utf-8") as infile:
             webpage = infile.read()
-            
+
         # Parse
         try:
-            soup = BeautifulSoup(webpage, 'html5lib')
+            soup = BeautifulSoup(webpage, "html5lib")
         except FeatureNotFound:
-            soup = BeautifulSoup(webpage, 'html.parser')
+            soup = BeautifulSoup(webpage, "html.parser")
 
         # Narrow down to relevant content
-        content = soup.select('[class~=article-content]')[0]
+        content = soup.select("[class~=article-content]")[0]
         title = clean_title(soup.title.get_text())
-        
+
         # Extract text
         text = []
-        for part in content.find_all('p'):
+        for part in content.find_all("p"):
 
             # Metadata
-            exists = [x.get('id') for x in part.find_all(True)]
-            if 'date' in exists:
-                date = part.find(id='date').get_text()
-                time = part.find(id='time').get_text()
-                ticker = part.find(class_='ticker').get_text()
+            exists = [x.get("id") for x in part.find_all(True)]
+            if "date" in exists:
+                date = part.find(id="date").get_text()
+                time = part.find(id="time").get_text()
+                ticker = part.find(class_="ticker").get_text()
                 continue
-            
+
             # Skip links
             links = part.find_all(href=True)
             if links:
                 continue
-            
+
             # Skip Motley Fool
-            if re.search(r'The Motley Fool.$', part.get_text()):
+            if re.search(r"The Motley Fool.$", part.get_text()):
                 continue
-            
+
             # Text
             text.append(part.get_text())
-        
+
         # Collect
-        results[title] = {'text':' '.join(text),
-                          'date':date,
-                          'time':time,
-                          'ticker':ticker}
-                
+        results[title] = {
+            "text": " ".join(text),
+            "date": date,
+            "time": time,
+            "ticker": ticker,
+        }
+
     return results
 
 
-
 def clean_title(string):
-    
+
     # Remove leading and trailing white space
-    string = re.sub(r'^\s+','', string)
-    string = re.sub(r'\s+$', '', string)
-    
+    string = re.sub(r"^\s+", "", string)
+    string = re.sub(r"\s+$", "", string)
+
     # Remove The Motley Fool
-    string = re.sub(r' \| [\s\S]+$', '', string)
-    
+    string = re.sub(r" \| [\s\S]+$", "", string)
+
     return string
 
 
-
 def preprocess(nested_dict, window_size=20):
-    
+
     # Make copy into which window of 22 words is pasted
     result = nested_dict.copy()
 
     # Loop
     for title, content in nested_dict.items():
-        
+
         # Access raw text
-        text_str = content['text']
-        
-        # Preprocess
-        text_str = re.sub(r'[^a-zA-Z ]', '', text_str.lower())
-        words = text_str.split()
-        
-        # TODO: Remove name of analysts
-        
-        # Bigrams
-        bigrams = [' '.join(x) for x in zip(words[0:], words[1:])]
-        
-        # Window of +/- 10 consecutive bigrams
-        window = list(zip(*[bigrams[i:] for i in range(window_size+1)]))
-        
-        result[title]['bigram_windows'] = window
-        result[title]['cleaned'] = words
-        
+        text_str = content["text"]
+
+        preproc_result = preprocess_text(text_str=text_str, window_size=window_size)
+
+        result[title].update(preproc_result)
+
     return result
 
 
+def preprocess_text(text_str, window_size=20):
+
+    result = {}
+
+    # Preprocess
+    text_str = re.sub(r"[^a-zA-Z ]", "", text_str.lower())
+    words = text_str.split()
+
+    # Bigrams
+    bigrams = [" ".join(x) for x in zip(words[0:], words[1:])]
+
+    # Window of +/- 10 consecutive bigrams
+    window = list(zip(*[bigrams[i:] for i in range(window_size + 1)]))
+
+    result["bigram_windows"] = window
+    result["cleaned"] = words
+
+    return result
+
+
+def parse_file_name(fname_str):
+    """
+    Function reads a case opinion file name which is in the format
+    "[case_id]_[opinion_num]" and returns a dict with the fname_str and the
+    case_id and opinion_num.
+    """
+    fname_str_parts = re.sub(".txt", "", fname_str)
+    fname_str_parts = fname_str_parts.split("_")
+    return {
+        "fname": fname_str,
+        "id": fname_str_parts[0],
+        "opinion_num": fname_str_parts[1],
+    }
--- code/run_score.py
+++ code/run_score.py
@@ -1,141 +1,147 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Aug 23 21:12:55 2020
-
-@author: markusschwedeler
-"""
-
-# Change me
-homedir = '/Users/markusschwedeler/Projects/firmlevelrisk/'
-
 # Modules
-import os
-os.chdir(homedir)
 import sys
-if not 'code/' in sys.path:
-    sys.path.append('code/')
+
+sys.path.append("./code/")
 import _helpers as h
 import re
 import pandas as pd
+import os
 
 # Files
-earningscall_dir = 'input/earningscall_transcripts/'
-output_dir = 'output/'
-sentimentwords_file = ('input/sentimentwords/'
-                       + 'LoughranMcDonald_MasterDictionary_2018.csv')
-riskwords_file = 'input/riskwords/synonyms.txt'
-polbigrams_file = 'input/political_bigrams/political_bigrams.csv'
+input_dir = "input/cases/"
+output_dir = "output/"
+output_fname = input_dir.split("/")[-2]
+# Word files
+sentimentwords_file = (
+    "input/sentimentwords/" + "LoughranMcDonald_MasterDictionary_2018.csv"
+)
+riskwords_file = "input/riskwords/synonyms.txt"
+polbigrams_file = "input/political_bigrams/political_bigrams.csv"
 
+"""1) Load auxiliary data sets"""
 
-#-----------------------------#
-# 1) Load auxiliary data sets #
-#-----------------------------#
 # Import positive and negative sentiment words, risk words, and collect all
 sentiment_words = h.import_sentimentwords(sentimentwords_file)
 risk_words = h.import_riskwords(riskwords_file)
-allwords = dict(sentiment_words, **{'risk':risk_words})
+allwords = dict(sentiment_words, **{"risk": risk_words})
 
 # Import political bigrams
 political_bigrams = h.import_politicalbigrams(polbigrams_file)
 
 # SarsCov2-related words
-sarscov2 = ['Coronavirus', 'Corona virus', 'coronavirus',
-            'Covid-19', 'COVID-19', 'Covid19', 'COVID19',
-            'SARS-CoV-2', '2019-nCoV']
-sarscov2_words = set([re.sub('[^a-z ]', '', x.lower()) for x in sarscov2])
+sarscov2 = [
+    "Coronavirus",
+    "Corona virus",
+    "coronavirus",
+    "Covid-19",
+    "COVID-19",
+    "Covid19",
+    "COVID19",
+    "SARS-CoV-2",
+    "2019-nCoV",
+]
+sarscov2_words = set([re.sub("[^a-z ]", "", x.lower()) for x in sarscov2])
 
+"""2) List case files"""
 
-#---------------------------------------------#
-# 2) Load and clean earnings call transcripts #
-#---------------------------------------------#
-# Parse text and metadata from HTML
-transcripts_raw = h.load_transcripts(earningscall_dir)
+# List cases in the input_dir
+input_files = os.listdir(input_dir)
+# Parse them to return the id and opinion_num
+all_files = [h.parse_file_name(file) for file in input_files]
 
-# Preprocess text and return window of 22 consecutive bigrams
-preprocessed = h.preprocess(transcripts_raw)
+"""3) Scoring"""
 
+# Loop through files
+scores_list = []
+for file in all_files:
 
-#----------#
-# 3) Score #
-#----------#
-# Note: For illustrative purposes, I don't call any custom function
+    # Read text
+    file_path = input_dir + file["fname"]
+    with open(file_path, "r") as f:
+        case_text = f.read()
 
-# Loop through transcripts
-scores = {}
-for title, content in preprocessed.items():
-    
-    print('Working on:', title)
-    scores[title] = {}
-    
+    # Preprocess text
+    case_text_proc = h.preprocess_text(text_str=case_text)
+
     # Access preprocessed windows of consecutive bigrams
-    windows = content['bigram_windows']
-    words = content['cleaned']
-    
+    windows = case_text_proc["bigram_windows"]
+    words = case_text_proc["cleaned"]
+
     # Total number of words (to normalize scores)
     totalwords = len(words)
-    
+
     ### A) Score unconditional scores
-    risk = len([word for word in words if word in allwords['risk']])
-    sentpos = len([word for word in words if word in allwords['positive']])
-    sentneg = len([word for word in words if word in allwords['negative']])
+    risk = len([word for word in words if word in allwords["risk"]])
+    sentpos = len([word for word in words if word in allwords["positive"]])
+    sentneg = len([word for word in words if word in allwords["negative"]])
     covid = len([word for word in words if word in sarscov2_words])
-    
+
     # Collect and prepare for conditional scores
-    scores[title] = {
-        'Risk':risk,
-        'Sentiment':sentpos-sentneg,
-        'Covid':covid,
-        'Pol':0,
-        'PRisk':0,
-        'PSentiment':0,
-        'Total words':totalwords
-        }
-    
+    scores = {
+        "Risk": risk,
+        "Sentiment": sentpos - sentneg,
+        "Covid": covid,
+        "Pol": 0,
+        "PRisk": 0,
+        "PSentiment": 0,
+        "Total words": totalwords,
+    }
+
     ### B) Score conditional scores
     # Loop through each windows
     for window in windows:
-    
+
         # Find middle ngram and check whether a "political" bigram
         middle_bigram = window[10]
         if middle_bigram not in political_bigrams:
             continue
-        tfidf = political_bigrams[middle_bigram]['tfidf']
-        
+        tfidf = political_bigrams[middle_bigram]["tfidf"]
+
         # Create word list for easy and quick access
         window_words = set([y for x in window for y in x.split()])
-        
+
         # If yes, check whether risk synonym in window
-        conditional_risk = (len([word for word in window_words
-                            if word in allwords['risk']]) > 0)
-        
+        conditional_risk = (
+            len([word for word in window_words if word in allwords["risk"]]) > 0
+        )
+
         # If yes, check whether positive or negative sentiment
-        conditional_sentpos = len([word for word in window_words
-                                   if word in allwords['positive']])
-        conditional_sentneg = len([word for word in window_words
-                                   if word in allwords['negative']])
-        
+        conditional_sentpos = len(
+            [word for word in window_words if word in allwords["positive"]]
+        )
+        conditional_sentneg = len(
+            [word for word in window_words if word in allwords["negative"]]
+        )
+
         # Weigh by tfidf
         conditional_risk = conditional_risk * tfidf
         conditional_sentpos = conditional_sentpos * tfidf
         conditional_sentneg = conditional_sentneg * tfidf
-        
+
         # Collect results
-        scores[title]['Pol'] += tfidf
-        scores[title]['PRisk'] += conditional_risk
-        scores[title]['PSentiment'] += (conditional_sentpos-
-                                                 conditional_sentneg)
-        
+        scores["Pol"] += tfidf
+        scores["PRisk"] += conditional_risk
+        scores["PSentiment"] += conditional_sentpos - conditional_sentneg
+
+    # Append scores to file dict and then add to scores_list
+    file.update(scores)
+    scores_list.append(file)
 
 # Collect in dataframe
-scores_df = pd.DataFrame().from_dict(scores, orient='index')
-scores_df.index.name = 'event name'
+scores_df = pd.DataFrame(scores_list)
+# Don't really need fname
+scores_df.drop(columns="fname", inplace=True)
 
 # Scale
-toscale = [x for x in scores_df.columns if x not in {'Total words'}]
+toscale = [
+    x
+    for x in scores_df.columns
+    if x not in ["Total words", "fname", "id", "opinion_num"]
+]
 for column in toscale:
-    scores_df[column] = scores_df[column]*100000*(1/scores_df['Total words'])
+    scores_df[column] = scores_df[column] * 100000 * (1 / scores_df["Total words"])
 
 # Write
-scores_df.to_csv(output_dir + 'earningscall_scores.tsv', sep='\t',
-                 encoding='utf-8')
+scores_df.to_csv(
+    output_dir + output_fname + ".tsv", sep="\t", encoding="utf-8", index=False
+)
